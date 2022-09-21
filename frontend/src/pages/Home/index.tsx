f6041e68751f891a7c7e280bd0489f933a5a66cd
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-09-21 16:50:38 -0300

    feat(section): create sectionHome

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,5 +1,27 @@
-import { HomeContainer } from './styles'
+import { Button, HomeContainer, HomeTextField, HomeWrapper } from './styles'
+
+import HomePhoto from '../../assets/homephoto.svg'
+import { BannerNumbers } from '../../components/BannerNumbers'
+import { Element } from 'react-scroll'
 
 export function Home() {
-  return <HomeContainer>Hello world!</HomeContainer>
+  return (
+    <HomeContainer>
+      <Element name="home">
+        <HomeWrapper>
+          <HomeTextField>
+            <span>BOAS-VINDAS A DOCTORCARE 👋</span>
+            <h1>Assistência médica simplificada para todos</h1>
+            <p>
+              Os médicos da DoctorCare vão além dos sintomas para tratar a causa
+              raiz de sua doença e proporcionar uma cura a longo prazo.
+            </p>
+            <Button>Agende sua consulta</Button>
+          </HomeTextField>
+          <img src={HomePhoto} loading="lazy" alt="" />
+        </HomeWrapper>
+      </Element>
+      <BannerNumbers />
+    </HomeContainer>
+  )
 }
--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -1,5 +1,86 @@
 import styled from 'styled-components'
 
 export const HomeContainer = styled.div`
-  font-size: xx-large;
+  display: flex;
+  flex-direction: column;
+
+  background-color: ${(props) => props.theme['gray-300']};
+`
+export const HomeWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+
+  background: ${(props) => props.theme['gray-400']};
+  gap: 3.1rem;
+  padding-bottom: 3.43rem;
+
+  @media (max-width: 900px) {
+    img {
+      width: 261.64px;
+      height: 342px;
+    }
+  }
+`
+
+export const HomeTextField = styled.div`
+  max-width: 55ch;
+
+  span {
+    color: ${(props) => props.theme['green-500']};
+
+    font-size: 0.875rem;
+    line-height: 150%;
+
+    margin-bottom: 1rem;
+  }
+
+  h1 {
+    color: ${(props) => props.theme['gray-900']};
+
+    font-size: 3.25rem;
+    font-weight: 700;
+    line-height: 130%;
+
+    @media (max-width: 900px) {
+      font-size: 2.125rem;
+    }
+  }
+
+  p {
+    color: ${(props) => props.theme['gray-500']};
+
+    font-size: 1.125rem;
+    font-weight: 400;
+    line-height: 150%;
+
+    margin-top: 1.5rem;
+    margin-bottom: 2rem;
+  }
+
+  @media (max-width: 900px) {
+    text-align: center;
+
+    margin: 2.5rem 1.5rem 0;
+  }
+`
+
+export const Button = styled.button`
+  color: ${(props) => props.theme.white};
+  background-color: ${(props) => props.theme['green-500']};
+  border: none;
+
+  font-size: 0.875rem;
+  font-weight: 700;
+  text-transform: uppercase;
+
+  border-radius: 40px;
+  padding: 1rem 2rem;
+
+  transition: all 0.2s;
+
+  &:hover {
+    background-color: ${(props) => props.theme['green-700']};
+  }
 `
